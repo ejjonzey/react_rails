c64484514c6c7cc248d705e98800ca2f260d6ee3
Ejjonzey <ejjonzey1982@gmail.com>
2017-09-05 13:47:44 -0400

    song index and show

--- app/controllers/api/songs_controller.rb
+++ app/controllers/api/songs_controller.rb
@@ -1,49 +1,17 @@
 class Api::SongsController < ApplicationController
     
     def index
-        @artist = Artists.find(params[:id])
-        @songs = @artist.Song.all
+        @artist = Artist.find(params[:artist_id])
+        @songs = @artist.songs.all
+      
         render json: @songs
     end
-
-    def create
-        @song = Song.new(song_params)
-        if @song.save
-            render json: @song
-        else
-            render json: {
-                message: 'error createing song'
-            }
-        end
-    end
-
+      
     def show
-        @song = Song.find(params[:id])
+        @artist = Artist.find(params[:artist_id])
+        @song = @artist.songs.find params[:id]
+      
         render json: @song
     end
-
-    def update
-        @song = Song.find(params[:id])
-        if @song.update!(song_params)
-        else
-         render json: {
-             message: 'error createing artist'
-         }
-     end
- end
-
-    def destroy
-        @song = Song.find(params[:id])
-        @song.destroy
-        render json:{
-            message: 'song successfully destroyed'
-        }
-    end
-
-    private
-    
-    def song_params
-        params.require(:song).permit(:title, :preview_url, :album)
-    end
-
+      
 end
