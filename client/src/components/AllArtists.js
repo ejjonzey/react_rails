d5c828ce15300c9fd9423fbbfad03aaeb664d2c4
Ejjonzey <ejjonzey1982@gmail.com>
2017-09-05 15:05:20 -0400

    artists are mounting on dom

--- client/src/components/AllArtists.js
+++ client/src/components/AllArtists.js
@@ -1,6 +1,33 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 class AllArtists extends Component {
+    constructor(){
+        super();
+        this.state = {
+            error: '',
+            artists:[]
+        }
+    }
+
+    componentWillMount(){
+    this._fetchArtists();
+    }
+
+    
+    _fetchArtists = async ()=>{
+        try{
+            const res = await axios.get('/api/artists');
+            const artists = res.data;
+            this.setState({artists})
+        }catch(err){
+            this.setState({error: err})
+        }
+    }
+
+        
+
+    
     render() {
         return (
             <div>
